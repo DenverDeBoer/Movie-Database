0d449e830eb765147a3fe5070eb3f62c62f74bad
Denver DeBoer <deboerde@eos09.cis.gvsu.edu>
2019-04-14 21:54:22 -0400

    Insert code for 4 tables

--- insert.sql
+++ insert.sql
@@ -1,1 +1,42 @@
+--User(UserID, Username, DateJoined)
+INSERT INTO Users VALUES (01,"CoJo","5 April 2019");
+INSERT INTO Users VALUES (02,"Owen","5 April 2019");
+INSERT INTO Users VALUES (03,"Halo Reach","5 April 2019");
+INSERT INTO Users VALUES (04,"Bambi","5 April 2019");
+INSERT INTO Users VALUES (05,"cinema.uncanned","5 April 2019");
+INSERT INTO Users VALUES (06,"Pol Pot","5 April 2019");
 --
+--Movie(MovieName, Movie_Length, Release_Date, Budget, Prequel, Sequel,SName)
+INSERT INTO Movie VALUES ("Spider-Man Homecoming",133,"7 July 2017",175000000,NULL,"Spider-Man Far From Home","Marvel Studios");
+INSERT INTO Movie VALUES ("Willard",100,"14 March 2003",22000000,NULL,NULL,"Lion's Gate Studios");
+INSERT INTO Movie VALUES ("The Room",99,"26 June 2003",6000000,NULL,NULL,"Wiseau Studios");
+INSERT INTO Movie VALUES ("The Killing of a Sacred Deer",121,"3 November 2017",2000000,NULL,NULL,"Element Pictures");
+INSERT INTO Movie VALUES ("Get Out",104,"24 February 2017",5000000,NULL,NULL,"Universal Pictures");
+INSERT INTO Movie VALUES ("Ant-Man and the Wasp",118,"6 July 2018",162000000,"Ant-Man","Captain Marvel","Marvel Studios");
+--
+--Artist(aID, aName, aBirthday, aAddress)
+INSERT INTO Artist VALUES (11,"Tom Holland","1 June 1996","London, UK");
+INSERT INTO Aritst VALUES (12,"Crispin Glover","20 April 1964","New York City, New York, USA");
+INSERT INTO Aritst VALUES (13,"Barry Keoghan","17 October 1992","Dublin, Ireland");
+INSERT INTO Aritst VALUES (14,"Greg Sestero","15 July 1978","Walnut Creek, California, USA");
+INSERT INTO Aritst VALUES (15,"Daniel Kaluuya","24 February 1989","London, UK");
+INSERT INTO Aritst VALUES (16,"Paul Rudd","6 April 1969","New Jersy, USA");
+INSERT INTO Aritst VALUES (21,"Jon Watts","28 June 1981","Fountain, Colorado, USA");
+INSERT INTO Aritst VALUES (22,"Glen Morgan","16 January 1947","San Francisco, California, USA");
+INSERT INTO Aritst VALUES (23,"Tommy Wiseau","3 October 1955","Poznan, Poland");
+INSERT INTO Aritst VALUES (24,"Yorgos Lanthimos","1 January 1973","Athens, Greece");
+INSERT INTO Aritst VALUES (25,"Jordan Peele","21 February 1979","New York City, New York, USA");
+INSERT INTO Aritst VALUES (26,"Peyton Reed","3 July 1964","Raleigh, North Carolina, USA");
+INSERT INTO Aritst VALUES (31,"Victoria Alonso","22 December 1965","Buenos Aires, Argentina");
+INSERT INTO Aritst VALUES (32,"Toby Emmerich","8 February 1963","New York City, New York, USA");
+INSERT INTO Aritst VALUES (33,"Drew Caffrey","13 August 1931","Santa Monica, California, USA");
+INSERT INTO Aritst VALUES (34,"Atilla Salih Yucer","12 February 1978","Bloemfontein, South Africa");
+INSERT INTO Aritst VALUES (35,"Jason Blum","20 February 1969","Los Angeles, California, USA");
+INSERT INTO Aritst VALUES (36,"Stan Lee","28 December 1922","Los Angeles, California, USA");
+--
+--Studio_Company(StuName,StuCEO,Website)
+INSERT INTO Aritst VALUES ("Marvel Studios","Avi Arad","www.marvel.com/movies");
+INSERT INTO Aritst VALUES ("Universal Studios","Ronald Meyer","www.universalstudioshollywood.com");
+INSERT INTO Aritst VALUES ("Element Pictures","Annette Waldron",NULL);
+INSERT INTO Aritst VALUES ("Wiseau Studios","Tommy Wiseau","wiseaustudios.com");
+INSERT INTO Aritst VALUES ("Lion's Gate Studios","Peter Strauss","www.lionsgate.com");
