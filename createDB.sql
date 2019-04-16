1eacf9a8221fda1473f2f7990f9b0b316dfaa4d9
SilasA <agnews@arch01.cis.gvsu.edu>
2019-04-16 18:52:38 -0400

    Working create and delete

--- createDB.sql
+++ createDB.sql
@@ -1,3 +1,4 @@
+--SET ECHO ON
 --
 -- Author: Silas Agnew, Denver DeBoer, Nicholas English
 -- Date: 15 April 2019
@@ -5,59 +6,60 @@
 -- Description:
 --
 CREATE TABLE Users (
-    uid int PRIMARY KEY,
+    userID int PRIMARY KEY,
     username varchar(255) NOT NULL,
     dateJoined varchar(255) NOT NULL
-)
+);
+--
+CREATE TABLE StudioCompany (
+    sName varchar(255) PRIMARY KEY,
+    sCEO varchar(255),
+    sWebsite varchar(255)
+);
 --
 CREATE TABLE Movie (
     movieName varchar(255) PRIMARY KEY,
     movieLength int,
     releaseDate varchar(255),
     budget int,
-    prequel varchar(255) FOREIGN KEY REFERENCES Movie(movieName),
-    sequel varchar(255) FOREIGN KEY REFERENCES Movie(movieName),
-    studioName varchar(255) FOREIGN KEY REFERENCES Studio(sName)
-)
+    prequel varchar(255) REFERENCES Movie(movieName),
+    sequel varchar(255) REFERENCES Movie(movieName),
+    studioName varchar(255) REFERENCES StudioCompany(sName)
+);
 --
 CREATE TABLE Artist (
     aID int PRIMARY KEY,
     aName varchar(255),
     aBirthday varchar(255),
     aAddress varchar(255)
-)
---
-CREATE TABLE StudioCompany (
-    sName varchar(255) PRIMARY KEY,
-    sCEO varchar(255),
-    sWebsite varchar(255)
-)
+);
 --
 CREATE TABLE UserReview (
-    uID int FOREIGN KEY REFERENCES User(uid),
-    mName varchar(255) FOREIGN KEY REFERENCES Movie(movieName),
+    userID int REFERENCES Users(userID),
+    mName varchar(255) REFERENCES Movie(movieName),
     dateReviewed varchar(255),
     score int,
-    CONSTRAINT UR PRIMARY KEY (uID, mName, dateReviewed),
-    CONSTRAINT SC CHECK(score >= 1 AND score <=15)
-)
+    CONSTRAINT UR PRIMARY KEY (userID, mName, dateReviewed),
+    CONSTRAINT SC CHECK(score >= -50 AND score <= 50)
+);
 --
 CREATE TABLE WorksOn (
-    aID int FOREIGN KEY REFERENCES Artist(aID),
-    mName varchar(255) FOREIGN KEY REFERENCES Movie(movieName)
-    role vharchar(255)
+    aID int REFERENCES Artist(aID),
+    mName varchar(255) REFERENCES Movie(movieName),
+    role varchar(255),
+    CONSTRAINT AM PRIMARY KEY (aID, mName), 
     CONSTRAINT RL CHECK(role IN ('Director', 'Actor', 'Producer'))
-)
+);
 --
 CREATE TABLE ContractWith (
-    aID int FOREIGN KEY REFERENCES Artist(aID),
-    sName varchar(255) FOREIGN KEY REFERENCES StudioCompany(sName),
+    aID int REFERENCES Artist(aID),
+    sName varchar(255) REFERENCES StudioCompany(sName),
     CONSTRAINT WO PRIMARY KEY (aID, sName)
-)
+);
 --
 CREATE TABLE MovieType (
-    movieName varchar(255) FOREIGN KEY REFERENCES Movie(movieName),
+    movieName varchar(255) REFERENCES Movie(movieName),
     genre varchar(255),
     CONSTRAINT MG PRIMARY KEY (movieName, genre)
-)
-
+);
+--
--- deleteDB.sql
+++ deleteDB.sql
@@ -1,6 +1,14 @@
 --
 -- Author: Silas Agnew, Denver DeBoer, Nicholas English
--- Date: 12 April 2019
+-- Date: 16 April 2019
 -- ------------------------------------------------------------------
 -- Description:
 --
+DROP TABLE Users CASCADE CONSTRAINTS;
+DROP TABLE Movie CASCADE CONSTRAINTS;
+DROP TABLE Artist CASCADE CONSTRAINTS;
+DROP TABLE StudioCompany CASCADE CONSTRAINTS;
+DROP TABLE UserReview CASCADE CONSTRAINTS;
+DROP TABLE WorksOn CASCADE CONSTRAINTS;
+DROP TABLE ContractWith CASCADE CONSTRAINTS;
+DROP TABLE MovieType CASCADE CONSTRAINTS;
