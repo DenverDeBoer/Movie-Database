95e2b7b6cf5895b0e15a4c7810e3d2c90e5a11b0
Nicholas English <englisni@arch10.cis.gvsu.edu>
2019-04-16 19:35:37 -0400

    Finished first query.

--- queryDB.sql
+++ queryDB.sql
@@ -8,16 +8,22 @@
 --
 --
 /* Query #1
-   For every artist who was in a movie that is a Horror film:
+   For every artist who was in a movie that is a Horror film with more than 5 reviews:
    We find the artists name, name of the movie, studio company's name, and the average
-   score received for a movie with more than 5 reviews. We will sort by average score.
+   score received for a movie. We will sort by average score.
 
    Completes #1, #4, #5 query types needed.
 */
-SELECT	 A.aName, W.MName, S.StuName, AVG(U.Score)
-FROM	 Artist A, Works_On W, Studio_Company S, User U, User_Review R, Movie M
-WHERE	 M.genre = 'Horror' AND W.aID = A.aID AND U.userID = R.userID AND 
-	 W.MName = M.MName AND M.SName = S.StuName
-GROUP BY W.MName
+SELECT	 A.aName, W.MName, S.SName, AVG(R.Score)
+FROM	 Artist A, WorksOn W, StudioCompany S, Users U, UserReview R, Movie M,
+	 MovieType T
+WHERE	 T.genre = 'Horror' AND W.aID = A.aID AND U.userID = R.userID AND 
+	 W.MName = M.MovieName AND M.StudioName = S.SName AND T.movieName = M.MovieName
+GROUP BY A.aName, W.MName, S.SName
 HAVING	 COUNT(*) > 4
-ORDER BY AVG(U.Score);
+ORDER BY AVG(R.Score);
+--
+--
+--
+/* Query #2 */
+   
