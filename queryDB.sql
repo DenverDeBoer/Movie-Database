6595aaad2de6b383e2be36939bc8bc134584cdc8
Nicholas English <englisni@arch10.cis.gvsu.edu>
2019-04-16 21:09:12 -0400

    Finished query 2 and 3.

--- queryDB.sql
+++ queryDB.sql
@@ -14,16 +14,35 @@
 
    Completes #1, #4, #5 query types needed.
 */
-SELECT	 A.aName, W.MName, S.SName, AVG(R.Score)
-FROM	 Artist A, WorksOn W, StudioCompany S, Users U, UserReview R, Movie M,
-	 MovieType T
-WHERE	 T.genre = 'Horror' AND W.aID = A.aID AND U.userID = R.userID AND 
-	 W.MName = M.MovieName AND M.StudioName = S.SName AND T.movieName = M.MovieName
+SELECT	 A.aName, W.Mname, S.SName, AVG(R.Score)
+FROM	 Artist A, WorksOn W, StudioCompany S, UserReview R, Movie M, MovieType T
+WHERE	 T.genre = 'Horror' AND W.aID = A.aID AND
+	 W.MName = M.MovieName AND M.StudioName = S.SName 
+	 AND T.movieName = M.MovieName AND R.Mname = M.MovieName
 GROUP BY A.aName, W.MName, S.SName
 HAVING	 COUNT(*) > 4
 ORDER BY AVG(R.Score);
 --
 --
 --
-/* Query #2 */
-   
+/* Query #2
+   For every artist, find an artist who lives in the same location.
+
+   Completes #2 query type needed.
+*/
+SELECT	 A1.aName, A2.aName, A1.aAddress
+FROM	 Artist A1, Artist A2
+WHERE	 A1.aName != A2.aName AND A1.aAddress = A2.aAddress AND A1.aID > A2.aID
+ORDER BY A1.aAddress;
+--
+--
+--
+/* Query #3
+   For every artist, find the studio name via outer join query.
+
+   Completes #9 query type needed.
+*/
+SELECT	 A.aName, C.sName
+FROM	 Artist A LEFT JOIN ContractWith C
+ON	 A.aID = C.aID
+ORDER BY C.sName;
